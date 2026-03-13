6e2da07ee4ce7d3861b13132f85b9e40298e6a38
chaehyeon42 <qpqp8899@naver.com>
2026-03-13 13:42:32 +0900

    departmentTable add

---
+++ src/main/java/com/employee/api/entity/Department.java
@@ -0,0 +1,28 @@
+package com.employee.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+
+//H2 db에 Department테이블 추가
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name" ,nullable = false)
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+}
